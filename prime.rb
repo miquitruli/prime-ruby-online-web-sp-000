d7865bc42b9cfa2938ae81666b513cbcefdee70f
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-10 19:14:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,10 @@
 
 
 def prime?(num)
-  for x in 2..num-1
-    if num % x == 0
+  for x in 2..(num-1)
+    if (num % x) == 0
       return false
-    else
-      return true
     end
   end
-  if num <2
-    return false
-  elsif num == 2
-    return true
-  end
+  return true
 end
